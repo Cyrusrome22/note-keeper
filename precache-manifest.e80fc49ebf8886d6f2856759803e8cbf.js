b909a943cb7a4de1353fb88291a9cb24fcaddd58
Cyrus Rome Dela Cruz <cdelacruz@pitx.com.ph>
2020-09-16 16:44:29 +0800

    Updates

---
+++ precache-manifest.e80fc49ebf8886d6f2856759803e8cbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d4181e5f7d2699082ad18116ec2524",
+    "url": "/note-keeper/index.html"
+  },
+  {
+    "revision": "a9b2ffe356b85750a015",
+    "url": "/note-keeper/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "896ee744cb13b78e431a",
+    "url": "/note-keeper/static/js/2.21ffaecc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/note-keeper/static/js/2.21ffaecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b2ffe356b85750a015",
+    "url": "/note-keeper/static/js/main.3ce80066.chunk.js"
+  },
+  {
+    "revision": "165995028603df8b8d33",
+    "url": "/note-keeper/static/js/runtime-main.8bcc9ef6.js"
+  }
+]);
